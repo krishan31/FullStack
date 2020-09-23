69615b89abd5fa6dc68f8aecc402a927ae3ae9f4
krishan31 <krishanarora009@hotmail.com>
2020-09-23 07:16:32 -0700

    Eception in Java Programming 23 - sep 20
    Eception in Java Programming 23 - sep 20

---
+++ Java Programming/src/Exception/Exception_1.java
@@ -0,0 +1,17 @@
+package Exception;
+import java.util.Scanner;
+
+public class Exception_1 {
+	public static void main(String args[]) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter A:");
+		int a = sc.nextInt();
+		System.out.println("Enter B:");
+		int b = sc.nextInt();
+		int c = a/b;
+		System.out.println("Division C: "+c);
+		System.out.println("System of code");
+		System.out.println("Program continue");
+	}
+}
